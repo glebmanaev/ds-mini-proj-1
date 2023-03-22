c1edac99f5b6f673ffa271832ca2757db8bcf6d5
glebmanaev <87305392+glebmanaev@users.noreply.github.com>
2023-03-22 21:04:10 +0200

    Create client.py

---
+++ client.py
@@ -0,0 +1,9 @@
+def set_symbol():
+    command = input("Set-symbol")
+    cell, symbol = command.split()[-1].split(',')
+    # request
+
+def list_board():
+    # send request
+    # print response
+    pass
