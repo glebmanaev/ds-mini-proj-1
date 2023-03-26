b419ce4886b3fe20de5ca218678361eee23533d7
glebmanaev <glebmanaev@gmail.com>
2023-03-26 18:48:32 +0300

    Co-authored-by: Dima <Dimonitr@users.noreply.github.com>

--- tictactoe_client.py
+++ tictactoe_client.py
@@ -1,3 +1,4 @@
+import time
 import grpc
 import tic_tac_toe_pb2
 import tic_tac_toe_pb2_grpc
@@ -11,13 +12,54 @@ def print_instructions():
     print("  set-time-out <target> <minutes>")
     print("  exit")
 
+def start_game(stub):
+    client_time = time.time()
+    response = stub.StartGame(tic_tac_toe_pb2.StartGameRequest(client_time=client_time))
+    print("Game started.")
+
+def check_winner(stub):
+    board = stub.ListBoard(tic_tac_toe_pb2.ListBoardRequest()).board_state
+    # Check for horizontal wins
+    for i in range(0, 9, 3):
+        if board[i] == board[i + 1] == board[i + 2] != "":
+            return board[i]
+    
+    # Check for vertical wins
+    for i in range(3):
+        if board[i] == board[i + 3] == board[i + 6] != "":
+            return board[i]
+    
+    # Check for diagonal wins
+    if board[0] == board[4] == board[8] != "":
+        return board[0]
+    if board[2] == board[4] == board[6] != "":
+        return board[2]
+    
+    # Check for draw
+    if "" not in board:
+        return "draw"
+    
+    # No winner yet
+    return ""
+
 def main():
+    global is_leader
     channel = grpc.insecure_channel("localhost:50051")
     stub = tic_tac_toe_pb2_grpc.TicTacToeStub(channel)
 
     print_instructions()
 
     while True:
+        if is_leader:
+            verdict = check_winner(stub)
+            if verdict != "":
+                if verdict == "draw":
+                    print("It's a draw!")
+                else:
+                    print(f"{verdict} wins!")
+                print("Restarting game...")
+                # start_game(stub)
+
         try:
             command = input("\n> ").strip().split()
 
@@ -31,12 +73,16 @@ def main():
             elif command[0].lower() == "set-symbol":
                 cell = int(command[1])
                 symbol = command[2]
-                response = stub.SetSymbol(tic_tac_toe_pb2.SetSymbolRequest(cell=cell, symbol=symbol))
+                while True:
+                    response = stub.SetSymbol(tic_tac_toe_pb2.SetSymbolRequest(cell=cell, symbol=symbol))
+                    if not bool(response.success):
+                        print("Cell already taken. Try again.")
+                        command = input("\n> Set-symbol ").strip().split()
+                        cell = int(command[1])
+                        symbol = command[2]
+                    else:
+                        break
                 print("Symbol set.")
-                if response.winner is not "":
-                    print(f"Winner/draw: {response.winner}")
-                    # Reset board
-                    stub.StartGame(tic_tac_toe_pb2.StartGameRequest())
 
             elif command[0].lower() == "list-board":
                 response = stub.ListBoard(tic_tac_toe_pb2.ListBoardRequest())
@@ -67,4 +113,5 @@ def main():
             print_instructions()
 
 if __name__ == "__main__":
+    is_leader = False
     main()
